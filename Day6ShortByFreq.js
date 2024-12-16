fd20a79bdb44ec62bbf527fd7a90fb3970592eda
ramgavate68 <ramgabate6825@gmail.com>
2024-12-17 00:18:22 +0530

    Day6

--- Day6ShortByFreq.js
+++ Day6ShortByFreq.js
@@ -1,3 +1,21 @@
-const frequencySort=(arr)=>{
-    
-}
+const frequencySort = (nums) => {
+    let freqMap = {};
+    for (let num of nums) {
+      freqMap[num] = (freqMap[num] || 0) + 1;
+    }
+    let sorted = nums.sort((a, b) => {
+      if (freqMap[a] !== freqMap[b]) {
+        return freqMap[a] - freqMap[b];
+      }
+      return b - a;
+    });
+  
+    return sorted;
+  };
+  
+  const nums = [1, 1, 2, 2, 2, 3];
+  const result = frequencySort(nums);
+  console.log(result);
+  
+  // Output: [3, 1, 1, 2, 2, 2]
+  
